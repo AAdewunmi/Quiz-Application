d71c0d22509648a2a870cb86e8869645b46bb620
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-15 11:40:25 +0000

    Third Commit: Implement get user input with input()

--- com.application/quiz.py
+++ com.application/quiz.py
@@ -1,1 +1,16 @@
 # quiz.py
+
+# Get user input with input()
+
+answer = input("When was the first known use of the word 'quiz'?")
+if answer == '1781':
+    print("Correct")
+else:
+    print(f"The answer is '1781', not {answer!r}")
+
+answer = input("Which builtin function can get information from the user?")
+if answer == 'input()':
+    print("Correct!")
+else:
+    print(f"The answer is 'input()', not {answer!r}")
+
