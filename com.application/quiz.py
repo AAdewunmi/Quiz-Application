803b7a5fe81456c1dbe1c8837e5cb5cafdc926f9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-15 12:10:43 +0000

    Fourth Commit: Implement storage of questions in List and Tuples

--- com.application/quiz.py
+++ com.application/quiz.py
@@ -2,15 +2,29 @@
 
 # Get user input with input()
 
-answer = input("When was the first known use of the word 'quiz'?")
-if answer == '1781':
-    print("Correct")
-else:
-    print(f"The answer is '1781', not {answer!r}")
+# answer = input("When was the first known use of the word 'quiz'?")
+# if answer == '1781':
+#     print("Correct")
+# else:
+#     print(f"The answer is '1781', not {answer!r}")
+#
+# answer = input("Which builtin function can get information from the user?")
+# if answer == 'input()':
+#     print("Correct!")
+# else:
+#     print(f"The answer is 'input()', not {answer!r}")
 
-answer = input("Which builtin function can get information from the user?")
-if answer == 'input()':
-    print("Correct!")
-else:
-    print(f"The answer is 'input()', not {answer!r}")
+# Store Questions and Answers in QUESTIONS data structure
 
+QUESTIONS = [
+    ("When was the first known use of the word 'quiz'", '1781'),
+    ("Which built-in function can get information from the user", 'input'),
+    ("Which keyword do you use to loop over a given list of elements", "for"),
+]
+
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
