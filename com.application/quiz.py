9239746231f71533d41812982eeb8af902bdb9e4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-16 16:54:21 +0000

    Eight Commit: Refactor to position question above answers

--- com.application/quiz.py
+++ com.application/quiz.py
@@ -20,15 +20,17 @@ QUESTIONS: dict[str, list[str]] = {
     ]
 }
 print('Welcome to Python Quiz', '\n', '****************')
-for question, alternatives in QUESTIONS.items():
+for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+    print(f"\nQuestion {num}: ")
+    print(f"{question}?")
     correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
-    for label, alternative in enumerate(sorted_alternatives):
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    for label, alternative in labeled_alternatives.items():
         print(f" {label}) {alternative}")
 
-    answer_label = int(input(f"{question}? "))
-    answer = sorted_alternatives[answer_label]
+    answer_label = input("\nChoice? ")
+    answer = labeled_alternatives.get(answer_label)
     if answer == correct_answer:
-        print("Correct!")
+        print("⭐️ Correct! ⭐️")
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
