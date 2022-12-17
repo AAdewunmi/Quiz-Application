125887f6cbbeb3288e876ad2d31ba77760cc78f8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-17 16:36:37 +0000

    Fourteen Commit: Implement random ordering of questions and the order of answer alternatives

--- com.application/quiz.py
+++ com.application/quiz.py
@@ -1,6 +1,9 @@
 # quiz.py
 
+import random
 from string import ascii_lowercase
+
+NUM_QUESTIONS_PER_QUIZ = 8
 
 QUESTIONS: dict[str, list[str]] = {
     "When was the first known use of the word 'quiz'": ['1781', '1771', '1871', '1881'],
@@ -39,13 +42,19 @@ QUESTIONS: dict[str, list[str]] = {
 
 }
 print('Welcome to Python Quiz', '\n', '****************')
+
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
 num_correct = 0
 num = 0
-for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+
+for num, (question, alternatives) in enumerate(questions, start=1):
     print(f"\nQuestion {num}:")
     print(f"{question}?")
     correct_answer = alternatives[0]
-    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    labeled_alternatives = dict(zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives))))
+
     for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
 
