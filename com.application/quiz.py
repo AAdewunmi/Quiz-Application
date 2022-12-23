a9d1501d4e8e695145e07b95cb5ff9cabc415300
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-23 10:35:18 +0000

    Thirty-Two Commit: Implement get_answers() to handle invalid answers from the user.

--- com.application/quiz.py
+++ com.application/quiz.py
@@ -32,7 +32,17 @@ def get_answers(question, alternatives, num_choices=1):
         answer = input(f"\nChoice{plural_s}? ")
         answers = set(answer.replace(",", " ").split())
 
-
+        # Handle invalid answers
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
+        if any((invalid := answer) not in labeled_alternatives for answer in answers):
+            print(
+                f"{invalid!r} is not a valid choice. "
+                f"Please use {', '.join(labeled_alternatives)}"
+            )
+            continue
         return [labeled_alternatives[answer] for answer in answers]
 
 
